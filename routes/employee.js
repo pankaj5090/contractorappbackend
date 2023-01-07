79365ecab273e2291cfa6e46c72633936eeeffd2
pankaj5090 <pankaj5090@gmail.com>
2023-01-07 17:02:45 +0530

    update and delete employee

--- routes/employee.js
+++ routes/employee.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const Employee = require("../models/Employee");
 const { body, validationResult } = require("express-validator");
 
+//with this request an employee will be created.
 router.post(
   "/",
   [
@@ -47,5 +48,78 @@ router.post(
     }
   }
 );
+
+router.get("/", async (req, resp) => {
+  try {
+    let employee = await Employee.find();
+    resp.json(employee);
+  } catch (error) {
+    console.error(error.message);
+    resp.status(500).send("Something went wrong");
+  }
+});
+
+router.put("/:id", async (req, resp) => {
+  try {
+    const {
+      name,
+      aadharCardNumber,
+      accountNumber,
+      esicNumber,
+      epfNumber,
+      ifscCode,
+      birthDate,
+    } = req.body;
+    const newEmployee = {};
+    if (name) {
+      newEmployee.name = name;
+    }
+    if (aadharCardNumber) {
+      newEmployee.aadharCardNumber = aadharCardNumber;
+    }
+    if (accountNumber) {
+      newEmployee.accountNumber = accountNumber;
+    }
+    if (esicNumber) {
+      newEmployee.esicNumber = esicNumber;
+    }
+    if (epfNumber) {
+      newEmployee.epfNumber = epfNumber;
+    }
+    if (ifscCode) {
+      newEmployee.ifscCode = ifscCode;
+    }
+    if (birthDate) {
+      newEmployee.birthDate = birthDate;
+    }
+    let employee = await Employee.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: newEmployee,
+      },
+      { new: true }
+    );
+    if (!employee) {
+      resp.status(404).send("Employee not found");
+    }
+    resp.json(employee);
+  } catch (error) {
+    console.error(error.message);
+    resp.status(500).send("Something went wrong");
+  }
+});
+
+router.delete("/:id", async (req, resp) => {
+  try {
+    let employee = await Employee.findByIdAndDelete(req.params.id);
+    if (!employee) {
+      resp.json({ Success: "Employee not found" });
+    }
+    resp.json({ Success: "Employee has been deleted", employee: employee });
+  } catch (error) {
+    console.error(error.message);
+    resp.status(500).send("Something went wrong");
+  }
+});
 
 module.exports = router;
